025dc659fed491545f09f2874cb3784ad1dbe65b
Jose Almada <jalmada.e@gmail.com>
2019-11-09 23:31:37 -0500

    Testoy viendo....

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send('Hola Esposi Te abo mucho, eres mi favorita!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+//proxy22.rt3.io:34912
