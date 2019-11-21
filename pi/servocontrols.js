1b1c7cd803aa72da7147ca4aac1657f5b00d1be7
jalmada <jalmada.e@gmail.com>
2019-11-20 23:38:15 -0500

    MAs tests

--- pi/servocontrols.js
+++ pi/servocontrols.js
@@ -1,13 +1,25 @@
 const Gpio = require('pigpio').Gpio;
-//const pi = Gpio.
-const pinNo = 4 //This is using the normal PCI positions not the Board
 
+const rpio = require('rpio');
+
+var options = {
+    gpiomem: false,          /* Use /dev/gpiomem */
+    mapping: 'gpio',    /* Use the P1-P40 numbering scheme */
+    mock: undefined,        /* Emulate specific hardware in mock mode */
+}
+
+rpio.init(options);
+
+const pinNo = 4 //This is using the normal PCI positions not the Board
 
 export default {
     MoveServo : (degreesPosition) => {
-        let servoPin = new Gpio(pinNo, {mode: Gpio.OUTPUT});
-        let modulation = Gpio.PWM(7, 60)
 
+        rpio.init(options);
+
+
+        let servoPin = new Gpio(pinNo, {mode: Gpio.OUTPUT});
+        let modulation = rpio.open(pinNo, rpio.PWM);
 
     }
 }
