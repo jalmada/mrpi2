b87b3cbdae482d896b8b8fd74aab2d33572f8361
jalmada <jalmada.e@gmail.com>
2019-11-20 20:26:09 -0500

    Test Node to turn on led

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Gpio  = require('pigpio').Gpio;
 var express = require('express');
 var app = express();
 
@@ -5,6 +6,23 @@ app.use(express.static('public'));
 
 app.get('/teabo', function (req, res) {
   res.send('Hola Esposi Te abo mucho, eres mi favorita!!');
+});
+
+app.post('/led', function (req, res) {
+  
+  let dutyCycle = 0;
+
+  const led = new Gpio (18, {mode: Gpio .OUTPUT});
+
+  setInterval(() => {
+    led.pwmWrite(dutyCycle);
+
+    dutyCycle += 5;
+    if (dutyCycle > 255) {
+      dutyCycle = 0;
+    }
+}, 20);
+
 });
 
 app.listen(3000, function () {
