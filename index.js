9cd90b07b268453eedb37a54a084281402861d52
jalmada <jalmada.e@gmail.com>
2019-11-20 20:50:21 -0500

    Added Led functionality

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-const Gpio  = require('pigpio').Gpio;
+var Gpio = require('onoff').Gpio; 
 var express = require('express');
 var app = express();
+var blinkInterval = null;
 
+var LED = new Gpio(24, 'out');;
 app.use(express.static('public'));
 
 app.get('/teabo', function (req, res) {
   res.send('Hola Esposi Te abo mucho, eres mi favorita!!');
 });
 
-app.post('/led', function (req, res) {
-  
-  let dutyCycle = 0;
+app.post('/ledon', function (req, res) {
+  blinkInterval = setInterval(blinkLED, 250);
+});
 
-  const led = new Gpio (18, {mode: Gpio .OUTPUT});
-
-  setInterval(() => {
-    led.pwmWrite(dutyCycle);
-
-    dutyCycle += 5;
-    if (dutyCycle > 255) {
-      dutyCycle = 0;
-    }
-}, 20);
-
+app.post('/ledoff', function (req, res) {
+  clearInterval(blinkInterval); // Stop blink intervals
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport GPIO to free resources
 });
 
 app.listen(3000, function () {
@@ -30,4 +25,10 @@ app.listen(3000, function () {
 });
 
 
-//proxy22.rt3.io:34912
+function blinkLED() { //function to start blinking
+  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
+}
